69a881500ed93dcbf372dd50b517d57b90afff1f
ArmaanRana0066 <armaanrana0066@gmail.com>
2025-03-27 20:53:56 +0530

    index.html

---
+++ readme.md
@@ -0,0 +1,15 @@
+Hello Devs,
+
+I am Kanwar. This is the showcase of my project Chanakya ai Chatbot.
+
+In this project I have used Html, CSS and JS. In this project I have fetched the API of Google's Gemini AI model.
+
+This project is responsive to all devices.
+
+That's it for the project you can use this Project for your College Finals, Assignment.
+
+But I recommend not to use this for your Placement Purpose, instead work on your skills.
+
+
+
+~ @Dev-Kanwar
---
+++ script.js
@@ -0,0 +1,91 @@
+let input = document.querySelector("#inputbox");
+let chatcontainer=document.querySelector(".chat-container");
+let btn=document.querySelector("#submit");
+let contbefore=document.querySelector(".chat-before");
+
+const url="https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAXr2a1WKcZrQY8GIjFdTMMh04seCx4ogc"
+
+let user={
+    data:null,
+
+}
+console.log(user.data);
+
+async function generateresponse(aichatbox){
+    let chatoutput=aichatbox.querySelector(".chat-output");
+    let requestoption={
+        method:"POST",
+        headers:{'Content-Type': 'application/json'},
+        body:JSON.stringify({
+            contents: [{
+              parts:[{text: user.data}]
+              }]
+             })
+    }
+    try{
+        let response=await fetch(url,requestoption);
+        let data=await response.json();
+        let apiresponse=data?.candidates[0].content.parts[0].text;
+        chatoutput.innerText=apiresponse;
+    }
+    catch(error){
+        console.log(error)
+    }
+    
+}
+
+function creatchat(html,classes){
+    let div=document.createElement("div");
+    div.innerHTML=html;
+    div.classList.add(classes);
+    return div;
+}
+
+function chatprocess(message){
+    user.data=message;
+    let html=`<img src="images/user-logo.png" alt="user image" id="user-img" width="50">
+    <div class="user-input">
+        ${user.data}
+    </div>`
+    input.value="";
+    let createchatbox= creatchat(html,"user-chat");
+    chatcontainer.appendChild(createchatbox);
+
+    setTimeout(()=>{
+        let html=`<img src="images/chanak-logo.png" id="aiprofile" alt="Chankya" width="50">
+        <div class="chat-output">
+        <img src="images/UDui.gif" class="load" alt="loading" width="80px" height="30px">
+        </div>`
+        let aichatbox=creatchat(html,"ai-chat");
+        chatcontainer.appendChild(aichatbox);
+        smoothScrollToBottom();
+        generateresponse(aichatbox);
+    },200)
+}
+function smoothScrollToBottom() {
+    chatcontainer.scrollTo({
+        top: chatcontainer.scrollHeight,
+        behavior: "smooth",
+    });
+}
+input.addEventListener("keydown",(e)=>{
+    if(e.key=="Enter"){
+       chatprocess(input.value);
+       contbefore.style.display="none";
+    }
+})
+btn.addEventListener("click",(e)=>{
+    if(input.value==""){
+        alert("Please Enter Something");
+        contbefore.style.display="block";
+    }
+    else{
+    chatprocess(input.value);
+    contbefore.style.display="none";
+    }
+})
+
+
+
+//  Author-> Ami Kanwar   
+//  github-> @Dev-Kanwar 
